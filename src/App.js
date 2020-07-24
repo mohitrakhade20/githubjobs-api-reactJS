3e921ffecd3b6973ac826e3791ace634e6ad2707
Mohit <mohitrakhade20@gmail.com>
2020-07-24 23:50:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import fetchJobs from "./fetchJobs";
+// import {Container} from 'react-bootstrap'
 
 function App() {
+  const { jobs, load, err } = fetchJobs();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>GITHUB Jobs</h1>
+      {load && <h1>Loading...</h1>}
+      {err && <h1>Error :/ May be you should refresh</h1>}
+      <h1>{jobs.length}</h1>
     </div>
   );
 }
